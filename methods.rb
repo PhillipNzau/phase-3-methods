fe2a0919b4057c94504aed36386f7fc46af4a861
phillipnzau <nzauonline78@gmail.com>
2023-06-19 12:57:25 +0300

    ruby methods

--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
 # Your code here!
+def greetProgranner 
+    puts "Hello, programmer!"
+end
+
+greetProgranner
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet("Phillip")
+
+def greetWithDefault(name:"programmer")
+    puts "Hello, #{name}!"
+end
+
+greetWithDefault
+
+def add(num1,num2)
+    num1 + num2
+end
+
+puts add(1,2)
+
+def halve(number)
+    # number.to_f / 2 if number.is_a?(Numeric)
+    if number.is_a?(String)
+        "null"   
+    else
+        number / 2.0
+    end
+end
+
+puts halve("number")
+puts halve(5)
